70a0ed9e551a069d92172c892f9068792130228e
Victor Véliz <ing.vj15@gmail.com>
2023-09-11 19:01:52 -0400

    Load Periods

--- src/context/DirectorContext.js
+++ src/context/DirectorContext.js
@@ -1,37 +1,42 @@
-import { createContext, useContext,useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useAuth } from "./AuthProvider";
 import { createPeriodRequest } from "../api/director";
 
 const DirectorContext = createContext();
 
 export const useDirectors = () => {
-    const context = useContext(DirectorContext);
+  const context = useContext(DirectorContext);
 
-    if (!context) {
-        throw new Error("useDirectors must be used within a DirectorProvider");
-    }
-    return context;
+  if (!context) {
+    throw new Error("useDirectors must be used within a DirectorProvider");
+  }
+  return context;
 }
 
-export function DirectorsProvider({ children })
-{
-    const [directors] = useState([]);
-    const { user } = useAuth()
-    
-    const addPeriod = async (data) => {
-      try {
-        await createPeriodRequest(user.token, data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+export function DirectorsProvider({ children }) {
+  const [directors] = useState([]);
+  const { user } = useAuth()
+  const [period, setPeriod] = useState([]);
 
-    return (
-        <DirectorContext.Provider value={{
-            directors,
-            addPeriod
-        }}>
-            {children}
-        </DirectorContext.Provider>
-    );
+  const addPeriod = async (data) => {
+    try {
+      const res = await createPeriodRequest(user.token, data);
+      if(res.status === 200)
+      {
+        setPeriod(res.data)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <DirectorContext.Provider value={{
+      directors,
+      addPeriod,
+      period
+    }}>
+      {children}
+    </DirectorContext.Provider>
+  );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,19 @@
-import "./home.scss"
-import Sidebar from "../../components/sidebar/Sidebar"
-import Navbar from '../../components/navbar/Navbar'
-import Widget from "../../components/widget/Widget"
-import { useTeachers } from "../../context/TeachersContext"
-import { useEffect } from "react"
-import {useStudents} from '../../context/StudentsContext'
-import {useUsers} from '../../context/UsersContext'
-import Swal from "sweetalert2"
-import { useDirectors } from "../../context/DirectorContext"
+import "./home.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
+import { useTeachers } from "../../context/TeachersContext";
+import { useEffect } from "react";
+import { useStudents } from "../../context/StudentsContext";
+import { useUsers } from "../../context/UsersContext";
+import Swal from "sweetalert2";
+import { useDirectors } from "../../context/DirectorContext";
 
 const Home = () => {
-
   const { teachers, getTeachers } = useTeachers();
   const { students, getStudents } = useStudents();
   const { users, getUsers } = useUsers();
-  const { addPeriod } = useDirectors();
+  const { addPeriod, period } = useDirectors();
 
   let new_period = null;
 
@@ -24,29 +23,31 @@ const Home = () => {
     getUsers();
   }, []);
 
-  const _addPeriod = () => {
-    Swal.fire({
-      title: 'Ingrese el periodo escolar:',
-      input: 'text',
-      inputValue: '',
-      inputAttributes: {
-        autocapitalize: 'off'
-      },
+  const _addPeriod = async () => {
+    const { value: data } = await Swal.fire({
+      title: "Ingrese el periodo escolar:",
+      html:
+        '<label>Periodo: </label><input type="text" id="periodo" class="swal2-input"><br>' +
+        '<label>Fecha Inicio: </label><input type="date" id="fecha_inicio" class="swal2-input"><br>' +
+        '<label>Fecha Fin: </label><input type="date" id="fecha_fin" class="swal2-input">',
+      focusConfirm: false,
       showCancelButton: true,
-      confirmButtonText: 'Actualizar',
+      confirmButtonText: "Procesar",
       showLoaderOnConfirm: true,
-      preConfirm: async (data) => {
-        new_period = await addPeriod(data);
+      preConfirm: async () => {
+        return {
+          periodo: document.getElementById("periodo").value,
+          fechaInicio: document.getElementById("fecha_inicio").value,
+          fechaCulminacion: document.getElementById("fecha_fin").value,
+        };
       },
-      allowOutsideClick: () => !Swal.isLoading()
-    }).then((result) => {
-      if (result === true) {
-        Swal.fire('Realizado', 'Proceso realizado con exito.', 'success');
-      } else {
-        Swal.close();
-      }
-    })
-  }
+      allowOutsideClick: () => !Swal.isLoading(),
+    });
+
+    if (data) {
+      const res = await addPeriod(data);
+    }
+  };
 
   return (
     <div className="home">
@@ -54,21 +55,27 @@ const Home = () => {
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          <Widget type='user' amount={users.length ?? 0} />
-          <Widget type='teacher' amount={teachers.length ?? 0} />
-          <Widget type='student' amount={students.length ?? 0} />
+          <Widget type="user" amount={users.length ?? 0} />
+          <Widget type="teacher" amount={teachers.length ?? 0} />
+          <Widget type="student" amount={students.length ?? 0} />
         </div>
         <div className="listContainer">
-          <div className="listTitle">Periodo Escolar Actual: </div>
-          <div className="listTitle">Lapso Actual: </div>
-          <button type="button"
-            onClick={ _addPeriod }
-            className="addButton"
-              >Agregar Periodo Escolar</button>
+          <div className="listTitle">
+            Periodo Escolar Actual: <b>{period.periodo}</b>
+          </div>
+          <div className="listTitle">
+            Fecha Inicio: <b>{period.fechaInicio}</b>
+          </div>
+          <div className="listTitle">
+            Fecha Culminación: <b>{period.fechaCulminacion}</b>
+          </div>
+          <button type="button" onClick={_addPeriod} className="addButton">
+            Periodo Escolar
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
